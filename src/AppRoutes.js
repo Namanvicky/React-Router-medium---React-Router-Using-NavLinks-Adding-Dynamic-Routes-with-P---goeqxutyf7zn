fdd9385a3bb85106960036a23a6f81b56ea012c6
Namanvicky <116458366+Namanvicky@users.noreply.github.com>
2023-01-06 19:22:27 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import NoteFound from "./Pages/NotFound";
 
-import { BackgroundColorChanger } from './Pages/BackgroundColorChanger'
-import { Calculator } from './Pages/Calculator'
-import { Index } from './Pages/Index'
-
-export const AppRoutes = () =>{
-    return (
-
-    )
-}
+const AppRoutes = () => {
+  return (
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="*" element={<NoteFound />}></Route>
+      </Routes>
+  );
+};
+export default AppRoutes;
